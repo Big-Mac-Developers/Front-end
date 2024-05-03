73b9794b5e27cdbdc82a4b56e7c1a4c18b57c648
cooi0004 <cooi0004@student.monash.edu>
2024-05-03 22:41:33 +1000

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1> BIG MAC DEVELOPERS</h1>
+      <h1 className="text-3xl font-bold underline"> BIG MAC DEVELOPERS</h1>
     </div>
   );
 }
