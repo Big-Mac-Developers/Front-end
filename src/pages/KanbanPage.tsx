4652bd31679f8f95f1d669c2d90bf0e74c02ac8b
joseph <limjoseph2004@gmail.com>
2024-05-04 20:54:46 +1000

    Add progress bar

--- src/pages/KanbanPage.tsx
+++ src/pages/KanbanPage.tsx
@@ -1,5 +1,6 @@
 import KanbanBoard from "@/components/KanbanBoard";
 import { NewTaskDialog } from "@/components/NewTaskDialog";
+import { Progress } from "@/components/ui/progress";
 import { useParams } from "react-router-dom";
 
 export default function KanbanPage() {
@@ -10,11 +11,14 @@ export default function KanbanPage() {
 
 	return (
 		<div className="flex flex-col gap-8 md:gap-12">
-			<div className="flex gap-8 items-center">
-				<h1 className="md:text-4xl text-2xl font-bold">
-					{"✨ Assignment " + id}
-				</h1>
-				<NewTaskDialog />
+			<div className="flex items-center justify-between">
+				<div className="flex gap-8">
+					<h1 className="md:text-4xl text-2xl font-bold">
+						{"✨ Assignment " + id}
+					</h1>
+					<NewTaskDialog />
+				</div>
+				<Progress value={66} className="w-[60%] mt-2" />
 			</div>
 
 			<KanbanBoard />
