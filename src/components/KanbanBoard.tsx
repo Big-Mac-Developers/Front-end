b3cc04036bf08f2d3214734f4c5b3a6895caa589
joseph <limjoseph2004@gmail.com>
2024-05-04 14:57:06 +1000

    Finish KanbanBoard 2x

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,77 +1,110 @@
-import KanbanCard from "@/components/KanbanCard";
-import KanbanHeader from "@/components/KanbanHeader";
-import { Reorder } from "framer-motion";
-import { CheckCheck, CircleAlert, SquareCheck } from "lucide-react";
+import {
+	DragDropContext,
+	Draggable,
+	Droppable,
+	DropResult,
+} from "@hello-pangea/dnd";
+// @ts-ignore
+import mockData from "../lib/mockData";
 import { useState } from "react";
+import KanbanCard from "@/components/KanbanCard";
+
+interface Task {
+	id: string;
+	title: string;
+}
+
+interface Section {
+	id: string;
+	title: string;
+	tasks: Task[];
+}
 
 export default function KanbanBoard() {
-	const [todoItems, setTodoItems] = useState([0, 1, 2]);
-	const [inProgressItems, setInProgressItems] = useState([0, 1, 2]);
-	const [doneItems, setDoneItems] = useState([0, 1, 2]);
+	const [data, setData] = useState<Section[]>(mockData);
+
+	const onDragEnd = (result: DropResult) => {
+		if (!result.destination) return;
+		const { source, destination } = result;
+
+		if (source.droppableId !== destination.droppableId) {
+			const sourceColIndex = data.findIndex(
+				(e) => e.id === source.droppableId
+			);
+			const destinationColIndex = data.findIndex(
+				(e) => e.id === destination.droppableId
+			);
+
+			const sourceCol = data[sourceColIndex];
+			const destinationCol = data[destinationColIndex];
+
+			const sourceTask = [...sourceCol.tasks];
+			const destinationTask = [...destinationCol.tasks];
+
+			const [removed] = sourceTask.splice(source.index, 1);
+			destinationTask.splice(destination.index, 0, removed);
+
+			data[sourceColIndex].tasks = sourceTask;
+			data[destinationColIndex].tasks = destinationTask;
+
+			setData([...data]);
+		}
+	};
 
 	return (
-		<div className="flex justify-center">
+		<DragDropContext onDragEnd={onDragEnd}>
 			<div className="flex flex-col justify-between w-full">
 				<div className="grid grid-cols-1 gap-x-8 gap-y-4 md:grid-cols-3">
-					{/* Todo */}
-					<Reorder.Group
-						values={todoItems}
-						onReorder={setTodoItems}
-						className="flex flex-col gap-y-4 no-scrollbar dark:bg-neutral-800/[0.2] bg-neutral-100 rounded-lg border p-4 drop-shadow-md"
-						layoutScroll
-						style={{ overflowY: "scroll" }}
-					>
-						<KanbanHeader title="📃 Todo">
-						{todoItems.map((item) => (
-							<Reorder.Item key={item} value={item}>
-								<KanbanCard
-									title="Title"
-									description="Description"
-									date={"04/05/2024"}
-								/>
-							</Reorder.Item>
-						))}
-					</Reorder.Group>
-					{/* In Progress */}
-					<Reorder.Group
-						values={inProgressItems}
-						onReorder={setInProgressItems}
-						className="flex flex-col gap-y-4 no-scrollbar dark:bg-neutral-800/[0.2] bg-neutral-100 rounded-lg border p-4 drop-shadow-md"
-						layoutScroll
-						style={{ overflowY: "scroll" }}
-					>
-						<KanbanHeader title="✏️ In Progress">
-						{inProgressItems.map((item) => (
-							<Reorder.Item key={item} value={item}>
-								<KanbanCard
-									title="Title"
-									description="Description"
-									date={"04/05/2024"}
-								/>
-							</Reorder.Item>
-						))}
-					</Reorder.Group>
-					{/* Done */}
-					<Reorder.Group
-						values={doneItems}
-						onReorder={setDoneItems}
-						className="flex flex-col gap-y-4 no-scrollbar dark:bg-neutral-800/[0.2] bg-neutral-100 rounded-lg border p-4 drop-shadow-md"
-						layoutScroll
-						style={{ overflowY: "scroll" }}
-					>
-						<KanbanHeader title="✔️ Done">
-						{doneItems.map((item) => (
-							<Reorder.Item key={item} value={item}>
-								<KanbanCard
-									title="Title"
-									description="Description"
-									date={"04/05/2024"}
-								/>
-							</Reorder.Item>
-						))}
-					</Reorder.Group>
+					{data.map((section) => (
+						<Droppable key={section.id} droppableId={section.id}>
+							{(provided) => (
+								<div
+									{...provided.droppableProps}
+									className="kanban__section dark:bg-neutral-800/[0.2] bg-neutral-100 p-4 rounded-lg border"
+									ref={provided.innerRef}
+								>
+									<div className="text-xl font-bold mb-4">
+										{section.title}
+									</div>
+									<div className="gap-2 flex flex-col">
+										{section.tasks.map((task, index) => (
+											<Draggable
+												key={task.id}
+												draggableId={task.id}
+												index={index}
+											>
+												{(provided, snapshot) => (
+													<div
+														ref={provided.innerRef}
+														{...provided.draggableProps}
+														{...provided.dragHandleProps}
+														style={{
+															...provided
+																.draggableProps
+																.style,
+															opacity:
+																snapshot.isDragging
+																	? "0.5"
+																	: "1",
+														}}
+													>
+														<KanbanCard
+															title={task.title}
+															description="Description"
+															date="01/01/2004"
+														/>
+													</div>
+												)}
+											</Draggable>
+										))}
+										{provided.placeholder}
+									</div>
+								</div>
+							)}
+						</Droppable>
+					))}
 				</div>
 			</div>
-		</div>
+		</DragDropContext>
 	);
 }
--- src/pages/KanbanPage.tsx
+++ src/pages/KanbanPage.tsx
@@ -1,6 +1,5 @@
 import KanbanBoard from "@/components/KanbanBoard";
 import { NewTaskDialog } from "@/components/NewTaskDialog";
-import KanbanTest from "@/components/kanbantest/kanban/KanbanTest";
 
 interface KanbanPageProps {
 	title: String;
@@ -9,14 +8,12 @@ interface KanbanPageProps {
 export default function KanbanPage({ title }: KanbanPageProps) {
 	return (
 		<div className="flex flex-col gap-12 md:mx-[20%] m-4 mt-10 md:mt-28">
-			{/* Title */}
 			<div className="flex gap-8 items-center">
 				<h1 className="md:text-4xl text-2xl font-bold">{title}</h1>
 				<NewTaskDialog />
 			</div>
 
-			{/* <KanbanBoard /> */}
-			<KanbanTest />
+			<KanbanBoard />
 		</div>
 	);
 }
